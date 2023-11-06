f5f6d4a64e494ed620ea1fa246d0c5b6d305980a
aimenfathima <116288137+AiMeN0425@users.noreply.github.com>
2023-11-06 21:14:37 +0530

    Update process_login.php

--- process_login.php
+++ process_login.php
@@ -1,16 +1,30 @@
-
-
 <?php
-session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-// Set a session variable to indicate authentication
-$_SESSION['authenticated'] = true;
+    // Create a database connection
+    include 'db_connect.php';
 
-// Debugging: Add an echo statement to check if this code is reached
-echo "Redirecting..."; 
+    // Prepare and execute the SQL statement to retrieve the user's hashed password
+    $sql = "SELECT username, password FROM users WHERE username = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->bind_result($db_username, $db_password);
+    $stmt->fetch();
 
-// Redirect to the reservation page
-header('Location: reservation.php');
-exit();
+    if (password_verify($password, $db_password)) {
+        // Password is correct
+        // Redirect to the reservation.php page
+        header("Location: reservation.php");
+        exit(); // Make sure to exit after the redirect
+    } else {
+        // Invalid username or password
+        echo '<script>alert("Invalid username or password");</script>';
+    }
+
+    $stmt->close();
+    $conn->close();
+}
 ?>
-
