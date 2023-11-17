1cf37500bb210889fc17baafd55d8ea4913f440f
aimenfathima <116288137+AiMeN0425@users.noreply.github.com>
2023-11-18 00:17:40 +0530

    Update process_transaction.php

--- process_transaction.php
+++ process_transaction.php
@@ -1,6 +1,24 @@
 <?php
 session_start(); // Start the session if not already started
 
+// Function to perform an aggregate query for the total charged amount
+function getTotalChargedAmount($flightId, $conn) {
+    $aggregateSql = "SELECT SUM(Charged_amount) AS total_charged_amount FROM Transaction WHERE Flight_ID = ?";
+    $aggregateStmt = $conn->prepare($aggregateSql);
+
+    if ($aggregateStmt) {
+        $aggregateStmt->bind_param("s", $flightId);
+        $aggregateStmt->execute();
+        $aggregateStmt->bind_result($totalChargedAmount);
+        $aggregateStmt->fetch();
+        $aggregateStmt->close();
+
+        return $totalChargedAmount;
+    } else {
+        return false;
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if the required POST parameters are set
     if (
@@ -9,41 +27,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         isset($_POST["flight_id"])
     ) {
         // Get the payment method, charged amount, and flight ID from the form
-        $payment_method = $_POST["transaction_type"];
-        $charged_amount = $_POST["charged_amount"];
-        $flight_id = $_POST["flight_id"];
+        $paymentMethod = $_POST["transaction_type"];
+        $chargedAmount = $_POST["charged_amount"];
+        $flightId = $_POST["flight_id"];
 
         // Create a database connection (include your db_connect.php)
         include 'db_connect.php';
 
         // Query the Flight table to retrieve the departure date (flight_date) for the selected flight
-        $sql_flight = "SELECT flight_date FROM Flight WHERE Flight_ID = ?";
-        $stmt_flight = $conn->prepare($sql_flight);
+        $sqlFlight = "SELECT flight_date FROM Flight WHERE Flight_ID = ?";
+        $stmtFlight = $conn->prepare($sqlFlight);
 
-        if ($stmt_flight) {
-            $stmt_flight->bind_param("s", $flight_id);
-            $stmt_flight->execute();
-            $stmt_flight->bind_result($departure_date); // Use departure_date as the variable to store flight_date
-            $stmt_flight->fetch();
-            $stmt_flight->close();
+        if ($stmtFlight) {
+            $stmtFlight->bind_param("s", $flightId);
+            $stmtFlight->execute();
+            $stmtFlight->bind_result($departureDate); // Use departureDate as the variable to store flight_date
+            $stmtFlight->fetch();
+            $stmtFlight->close();
 
             // Insert the transaction details into the Transaction table with the retrieved departure date
-            $sql = "INSERT INTO Transaction (Ts_type, Departure_date, Booking_date, Charged_amount, Flight_ID)
+            $sqlTransaction = "INSERT INTO Transaction (Ts_type, Departure_date, Booking_date, Charged_amount, Flight_ID)
                     VALUES (?, ?, NOW(), ?, ?)";
-            $stmt = $conn->prepare($sql);
+            $stmtTransaction = $conn->prepare($sqlTransaction);
 
-            if ($stmt) {
+            if ($stmtTransaction) {
                 // Bind the parameters and execute the query
-                $stmt->bind_param("ssds", $payment_method, $departure_date, $charged_amount, $flight_id);
+                $stmtTransaction->bind_param("ssds", $paymentMethod, $departureDate, $chargedAmount, $flightId);
 
-                if ($stmt->execute()) {
-                    echo '<script>alert("Payment successful. Thank you for booking your flight!"); window.location.href = "home.php"; </script>';
+                if ($stmtTransaction->execute()) {
+                    // Call the function to get the total charged amount for the specific flight
+                    $totalChargedAmount = getTotalChargedAmount($flightId, $conn);
+
+                    if ($totalChargedAmount !== false) {
+                        // Display a success alert with the total charged amount
+                        echo '<script>alert("Payment successful. Thank you for booking your flight!\nTotal Charged Amount: ' . $totalChargedAmount . '"); window.location.href = "home.php"; </script>';
+                    } else {
+                        echo '<script>alert("Error in calculating the total charged amount.");</script>';
+                    }
                 } else {
+                    // Display a failure alert
                     echo '<script>alert("Payment failed. Please try again later.");</script>';
                 }
 
                 // Close the prepared statement
-                $stmt->close();
+                $stmtTransaction->close();
             } else {
                 echo '<script>alert("Error in preparing the SQL statement for Transaction.");</script>';
             }
@@ -59,6 +86,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo '<script>alert("Invalid request. Please submit the form.");</script>';
 }
-
-
-?>
+?> 
